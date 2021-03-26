8683cceb4870a58f966abc948c213b981171b888
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-03-26 15:06:35 +0530

    Added : results of Computation Value in Descending and Ascending Order

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -2,6 +2,44 @@
 
 declare -A DictResult
 
+
+function DescendingSort(){
+temp=0
+for ((i=1; i<=${#ArrayResult[@]}; i++))
+do
+   for ((j=$(($i+1)); j<=${#ArrayResult[@]}; j++))
+   do
+      if [ ${ArrayResult[$i]} -lt ${ArrayResult[$j]} ]
+      then
+         temp=${ArrayResult[$i]}
+         ArrayResult[$i]=${ArrayResult[$j]}
+         ArrayResult[$j]=$temp
+      fi
+   done
+done
+
+echo "Array in Descending Order : ${ArrayResult[@]}"
+}
+
+function AscendingSort(){
+temp=0
+for ((i=1; i<=${#ArrayResult[@]}; i++))
+do
+   for ((j=$(($i+1)); j<=${#ArrayResult[@]}; j++))
+   do
+      if [ ${ArrayResult[$i]} -gt ${ArrayResult[$j]} ]
+      then
+         temp=${ArrayResult[$i]}
+         ArrayResult[$i]=${ArrayResult[$j]}
+         ArrayResult[$j]=$temp
+      fi
+   done
+done
+
+echo "Array in Ascending Order : ${ArrayResult[@]}"
+}
+
+
 echo "Enter three inputs"
 read a b c
 echo $a $b $c
@@ -19,7 +57,7 @@ DictResult[2]=$secondCompute
 DictResult[3]=$thirdCompute
 DictResult[4]=$fourthCompute
 
-echo ${DictResult[@]}
+echo "values in Dictionary : $DictResult[@]}"
 
 
 for ((i=1; i<=${#DictResult[@]}; i++))
@@ -27,20 +65,8 @@ do
 	ArrayResult[$i]=${DictResult[$i]}
 done
 
-echo ${ArrayResult[@]}
+echo "value in Array : ${ArrayResult[@]}"
 
-temp=0
-for ((i=1; i<=${#ArrayResult[@]}; i++))
-do
-   for ((j=$(($i+1)); j<=${#ArrayResult[@]}; j++))
-   do
-      if [ ${ArrayResult[$i]} -lt ${ArrayResult[$j]} ]
-      then
-         temp=${ArrayResult[$i]}
-         ArrayResult[$i]=${ArrayResult[$j]}
-         ArrayResult[$j]=$temp
-      fi
-   done
-done
+echo $(DescendingSort)
 
-echo "Array in Descending Order : ${ArrayResult[@]}"
+echo $(AscendingSort)
