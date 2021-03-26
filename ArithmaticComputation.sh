b731d77c501515f747be13fd2869ac49f25e9293
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-03-26 14:33:19 +0530

    Added : Store the results in

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+declare -A DictResult
+
 echo "Enter three inputs"
 read a b c
 echo $a $b $c
@@ -11,3 +13,10 @@ secondCompute=$(($a*$b+$c))
 thirdCompute=$(($c+$a/$b))
 
 fourthCompute=$(($a%$b+$c))
+
+DictResult[1]=$firstCompute
+DictResult[2]=$secondCompute
+DictResult[3]=$thirdCompute
+DictResult[4]=$fourthCompute
+
+echo ${DictResult[@]}
